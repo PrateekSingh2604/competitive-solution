f2bb1473d11de1097128a4f7839f78b36e963617
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2024-11-02 11:59:02 +0530

    Update 1957. Delete Characters to Make Fancy String_leetcode.java variable changed

--- 1957. Delete Characters to Make Fancy String_leetcode.java
+++ 1957. Delete Characters to Make Fancy String_leetcode.java
@@ -1,6 +1,5 @@
 class Solution {
     public String makeFancyString(String s) {
-        int n = s.length();
         StringBuilder sb = new StringBuilder();
         for(char c: s.toCharArray()){
             if((sb.length() < 2) || sb.charAt(sb.length()-1) != c || sb.charAt(sb.length() - 2) != c){
