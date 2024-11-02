264da17dac2aedd156ba35cb7d0f9e1720d70cf0
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2024-11-02 11:56:24 +0530

    Create 2490. Circular Sentence_leetcode.java created

---
+++ 2490. Circular Sentence_leetcode.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        int n = sentence.length();
+        for(int i=0; i<n; ++i){
+            if(sentence.charAt(i) == ' ' && sentence.charAt(i-1) != sentence.charAt(i+1)){
+                return false;
+            } 
+        }
+        return sentence.charAt(0) == sentence.charAt(n-1);
+    }
+} 
