41efc59fe727aa2dd0ba4e31deb6c30cb03f1b28
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-02-03 11:41:05 +0530

    Create 3105. Longest Strictly Increasing or Strictly Decreasing Subarray_leetcode.java created

---
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray_leetcode.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length;
+        int result = 1;
+
+        int count = 1;
+        for(int i = 0; i<n-1; i++){
+            if(nums[i]<nums[i+1]){
+                count++;
+            }else{
+                result = Math.max(result, count);
+                count = 1;
+            }
+        }
+        result = Math.max(result, count);
+        count = 1;
+        for(int i = 0; i<n-1; i++){
+            if(nums[i]>nums[i+1]){
+                count++;
+            }else{
+                result = Math.max(result, count);
+                count = 1;
+            }
+        }
+        result = Math.max(result, count);
+        return result;
+    }
+}
