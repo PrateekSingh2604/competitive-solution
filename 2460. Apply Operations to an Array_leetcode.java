21ef69f6a85b9ee408c1eb303976e1c2842d27f9
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-03-01 11:03:25 +0530

    Create 2460. Apply Operations to an Array_leetcode.java created

---
+++ 2460. Apply Operations to an Array_leetcode.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for(int i = 0; i<n-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        int j = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                result[j] = nums[i];
+                j++;
+            }
+        }
+        return result;
+    }
+} 
+
