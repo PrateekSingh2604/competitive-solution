c95f6ebc6925e92fbc6be0f0a272bc11765296f4
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-03-31 12:32:42 +0530

    Create 2551. Put Marbles in Bags_leetcode.java create

---
+++ 2551. Put Marbles in Bags_leetcode.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        int[] pairs = new int[n-1]; 
+
+        for(int i = 0; i<n-1; i++){
+            pairs[i] = weights[i] + weights[i+1];
+        }
+
+        Arrays.sort(pairs);
+        long min = 0, max = 0;
+        for(int i = 0; i<k-1; i++){
+            min += pairs[i];
+            max += pairs[n-2-i];
+        }
+        return max-min;
+        
+    }
+}
