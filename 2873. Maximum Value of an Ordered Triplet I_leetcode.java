672e29c3d285240e993d4c3c0bc2fd1294f3793f
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-04-02 11:17:41 +0530

    Create 2873. Maximum Value of an Ordered Triplet I_leetcode.java created

---
+++ 2873. Maximum Value of an Ordered Triplet I_leetcode.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long result = 0;
+        int maxDiff = 0;
+        int maxNum = 0;
+        
+        for(int num: nums){
+            result = Math.max(result, (long) maxDiff*num);
+            maxDiff = Math.max(maxDiff, maxNum - num);
+            maxNum = Math.max(maxNum, num);
+        }
+        return result;
+    }
+}
