1b9587db35f21c590797f80eccba113f3fb7a1e7
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-06-04 13:56:30 +0530

    Create 3403. Find the Lexicographically Largest String From the Box I_leetcode.java created

---
+++ 3403. Find the Lexicographically Largest String From the Box I_leetcode.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String answerString(String word, int numFriends) {
+        int n = word.length();
+        if (numFriends == 1) {
+            return word;
+        }
+        int longestPossible = n - (numFriends - 1);
+        String result = "";
+
+        for(int i = 0; i < n; i++){
+            int canTakeLength = Math.min(longestPossible, n-i);
+            result = result.compareTo(word.substring(i, i+canTakeLength))>0?result:word.substring(i, i+canTakeLength); 
+        }
+        return result;
+    }
+}
