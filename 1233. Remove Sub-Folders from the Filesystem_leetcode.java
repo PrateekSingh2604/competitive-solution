4d85a2ebef6e85be1bb1b5e3eaa1847591690e04
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-07-19 12:03:27 +0530

    Update 1233. Remove Sub-Folders from the Filesystem_leetcode.java

--- 1233. Remove Sub-Folders from the Filesystem_leetcode.java
+++ 1233. Remove Sub-Folders from the Filesystem_leetcode.java
@@ -1,4 +1,32 @@
 class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        HashSet<String> f = new HashSet<>();
+        List<String> ans = new ArrayList<>();
+
+        for(String s: folder){
+            f.add(s);
+            ans.add(s);
+        }
+
+        for(String s: folder){
+            String check = "";
+            for(int i = 0; i<s.length(); i++){
+                check = check + "" + s.charAt(i);
+                if(f.contains(check) && i<s.length()-1 && s.charAt(i+1) == '/'){
+                    System.out.println(check);
+                    ans.remove(s);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+class Solution {
   public List<String> removeSubfolders(String[] folder) {
     List<String> ans = new ArrayList<>();
     String prev = "";
