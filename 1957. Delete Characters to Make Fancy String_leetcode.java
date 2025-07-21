d0459d6147ccd7295c9d3962b914f2fa1905cf0b
Prateek <108998200+PrateekSingh2604@users.noreply.github.com>
2025-07-21 14:17:58 +0530

    Update 1957. Delete Characters to Make Fancy String_leetcode.java

--- 1957. Delete Characters to Make Fancy String_leetcode.java
+++ 1957. Delete Characters to Make Fancy String_leetcode.java
@@ -10,3 +10,23 @@ class Solution {
         return new String(sb);
     }
 }
+
+
+class Solution {
+    public String makeFancyString(String s) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder();
+        ans.append(s);
+        
+        int i = 0;
+        while(i < (ans.length()-2)){
+            if(ans.charAt(i) == ans.charAt(i+1) && ans.charAt(i+1) == ans.charAt(i+2)){
+                ans.deleteCharAt(i+2);
+                continue;
+            }
+
+            i++;
+        }
+        return new String(ans);
+    }
+}
